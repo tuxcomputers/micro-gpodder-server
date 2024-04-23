03a5ff97324f073250107946a10d15cb8d3401a2
Harry Phillips <harry.phillips@fostermoore.com>
2024-04-23 14:29:43 +1000

    Rename tables and ID's

--- server/inc/schema.sql
+++ server/inc/schema.sql
@@ -1,40 +1,40 @@
-CREATE TABLE users (
-	id INTEGER NOT NULL PRIMARY KEY,
+CREATE TABLE user (
+	user_id INTEGER NOT NULL PRIMARY KEY,
 	name TEXT NOT NULL,
 	password TEXT NOT NULL
 );
 
-CREATE UNIQUE INDEX users_name ON users (name);
+CREATE UNIQUE INDEX UN1_users_name ON users (name);
 
-CREATE TABLE devices (
-	id INTEGER NOT NULL PRIMARY KEY,
-	user INTEGER NOT NULL REFERENCES users (id) ON DELETE CASCADE,
-	deviceid TEXT NOT NULL,
+CREATE TABLE device (
+	device_id INTEGER NOT NULL PRIMARY KEY,
+	user INTEGER NOT NULL REFERENCES users (user_id) ON DELETE CASCADE,
+	devicename TEXT NOT NULL,
 	data TEXT
 );
 
-CREATE UNIQUE INDEX deviceid ON devices (deviceid, user);
+CREATE UNIQUE INDEX UN1_devicename ON device (devicename, user);
 
-CREATE TABLE subscriptions (
-	id INTEGER NOT NULL PRIMARY KEY,
-	user INTEGER NOT NULL REFERENCES users (id) ON DELETE CASCADE,
+CREATE TABLE subscription (
+	subscription_id INTEGER NOT NULL PRIMARY KEY,
+	user INTEGER NOT NULL REFERENCES users (user_id) ON DELETE CASCADE,
 	url TEXT NOT NULL,
 	deleted INTEGER NOT NULL DEFAULT 0,
 	changed INTEGER NOT NULL,
 	data TEXT
 );
 
-CREATE UNIQUE INDEX subscription_url ON subscriptions (url, user);
+CREATE UNIQUE INDEX UN1_subscription_url ON subscription (url, user);
 
-CREATE TABLE episodes_actions (
-	id INTEGER NOT NULL PRIMARY KEY,
-	user INTEGER NOT NULL REFERENCES users (id) ON DELETE CASCADE,
-	device_id INTEGER NOT NULL REFERENCES devices (id) ON DELETE CASCADE,
-	subscription INTEGER NOT NULL REFERENCES subscriptions (id) ON DELETE CASCADE,
+CREATE TABLE episodes_action (
+	episodes_action_id INTEGER NOT NULL PRIMARY KEY,
+	user_id INTEGER NOT NULL REFERENCES user (user_id) ON DELETE CASCADE,
+	device_id INTEGER NOT NULL REFERENCES device (devices_id) ON DELETE CASCADE,
+	subscription_id INTEGER NOT NULL REFERENCES subscription (subscription_id) ON DELETE CASCADE,
 	url TEXT NOT NULL,
 	changed INTEGER NOT NULL,
 	action TEXT NOT NULL,
 	data TEXT
 );
 
-CREATE INDEX episodes_idx ON episodes_actions (user, action, changed);
+CREATE INDEX IX1_episodes_idx ON episodes_action (user, action, changed);
